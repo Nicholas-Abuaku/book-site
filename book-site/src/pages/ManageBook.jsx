b890027b692567a4c64483252335ebb481662141
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2023-11-03 11:28:11 +0000

    Added "add book" button to manage page

--- book-site/src/pages/ManageBook.jsx
+++ book-site/src/pages/ManageBook.jsx
@@ -4,8 +4,9 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Button from "react-bootstrap/esm/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export const ManageBook = () => {
+  let navigate = useNavigate();
   const [books, setBooks] = useState([]);
   useEffect(() => {
     const fetchAllBooks = async () => {
@@ -28,41 +29,54 @@ export const ManageBook = () => {
     }
   };
 
-  return (
-    <Table style={{ marginTop: 40 }}>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.map((book) => {
-          return (
-            <tr key={book.id}>
-              <td>{book.id}</td>
-              <td>{book.title}</td>
-              <td>{book.author}</td>
+  function addBookNavigate() {
+    navigate("/addbook");
+  }
 
-              <td>
-                <ButtonGroup>
-                  <Button className="btn-warning">
-                    <Link to={`/update/${book.id}`}>Edit</Link>
-                  </Button>
-                  <Button
-                    className="btn-danger"
-                    onClick={() => handleDelete(book.id)}
-                  >
-                    Delete
-                  </Button>
-                </ButtonGroup>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+  return (
+    <>
+      <Button
+        className="btn-success"
+        style={{ float: "right" }}
+        onClick={addBookNavigate}
+      >
+        + Add Book
+      </Button>
+      <Table style={{ marginTop: 40 }}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => {
+            return (
+              <tr key={book.id}>
+                <td>{book.id}</td>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+
+                <td>
+                  <ButtonGroup>
+                    <Button className="btn-warning">
+                      <Link to={`/update/${book.id}`}>Edit</Link>
+                    </Button>
+                    <Button
+                      className="btn-danger"
+                      onClick={() => handleDelete(book.id)}
+                    >
+                      Delete
+                    </Button>
+                  </ButtonGroup>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
   );
 };
